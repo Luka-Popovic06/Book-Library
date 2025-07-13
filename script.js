f7f0a8b26e29abc5577a3ca366896f1230cf979d
Luka Popovic <lukapopovic0111@gmail.com>
2025-07-13 15:59:01 +0200

    Update book edit mode

--- script.js
+++ script.js
@@ -30,6 +30,9 @@ function bookManager() {
   return { pushBook, getBook, deleteBook, findBook };
 }
 function bookCreator(bookTitle, bookAutor, bookNumber, bookCheck) {
+  const updateName = newName => (name = newName);
+  const updateAutor = newAutor => (autor = newAutor);
+  const updateNumberOfPages = newNumber => (numberOfPages = newNumber);
   let id = crypto.randomUUID();
   let name = bookTitle;
   let autor = bookAutor;
@@ -43,9 +46,6 @@ function bookCreator(bookTitle, bookAutor, bookNumber, bookCheck) {
   const getIsCheck = () => checked;
   const setIsCheck = value => (checked = value);
   //
-  const updateName = newName => (name = newName);
-  const updateAutor = newAutor => (autor = newAutor);
-  const updateNumberOfPages = newNumber => (numberOfPages = newNumber);
   return {
     getId,
     getName,
@@ -81,18 +81,23 @@ autor.addEventListener('input', function (e) {
 numberOfPages.addEventListener('input', function (e) {
   nPages = e.target.value;
 });
+
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-  console.log(check);
   const newBook = bookCreator(titleB, autorB, nPages, check);
   manager.pushBook(newBook);
   const books = manager.getBook();
   makeList(books);
 });
+//MAKE LIST
 function makeList(books) {
   ulBox.innerHTML = '';
+  if (books.length === 0) {
+    addMargin();
+    return;
+  }
   books.forEach(function (book) {
-    const html = `<li class="list-item" id=${book.getId()}>
+    const html = `<li class="list-item" id=${book.getId()} >
             <p class="card-book-name">${book.getName()}</p>
             <p class="card-book-autor">${book.getAutor()}</p>
             <p class="card-page-number">${book.getNumberOfPages()}</p>
@@ -102,7 +107,8 @@ function makeList(books) {
               }</button>
               <button type="button" class="btn-delete">Delete Book</button>
               <button type="button" class="btn-edit">Edit</button>
-            </div>`;
+            </div></li>`;
+
     ulBox.insertAdjacentHTML('afterbegin', html);
     form.classList.add('hidden');
     overlay.classList.add('hidden');
@@ -110,71 +116,75 @@ function makeList(books) {
   });
 }
 
+//EDIT MODE
+function editMode(book) {
+  const list = document.getElementById(book.getId());
+  list.innerHTML = '';
+  list.classList.add('list-item');
+  const html = `
+            <input type="text" class="input-mode-edit-name">
+            <input type="text" class="input-mode-edit-autor">
+            <input type="text" class="input-mode-edit-pagesnumber">
+            <div class="card-btn">
+              <button type="button" class="btn-read_un">${
+                book.getIsCheck() ? 'Read ✔️' : 'Unread ❌'
+              }</button>
+              <button type="button" class="delete-btn">Delete Book</button>
+              <button type="button" class="finish-edit">Finish Editing</button>
+            </div>`;
+  list.insertAdjacentHTML('afterbegin', html);
+
+  const name = document.querySelector('.input-mode-edit-name');
+  const autor = document.querySelector('.input-mode-edit-autor');
+  const numberOfPages = document.querySelector('.input-mode-edit-pagesnumber');
+
+  name.value = `${book.getName()}`;
+  autor.value = `${book.getAutor()}`;
+  numberOfPages.value = `${book.getNumberOfPages()}`;
+
+  name.addEventListener('input', function (e) {
+    book.updateName(e.target.value);
+  });
+  autor.addEventListener('input', function (e) {
+    book.updateAutor(e.target.value);
+  });
+  numberOfPages.addEventListener('input', function (e) {
+    book.updateNumberOfPages(e.target.value);
+  });
+
+  form.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+//UL BOX
+ulBox.addEventListener('click', function (e) {
+  const li = e.target.closest('.list-item');
+  const books = manager.getBook();
+  const oneBook = books.find(b => b.getId() === li.id);
+  if (e.target.closest('.btn-delete')) {
+    manager.deleteBook(li.id);
+    makeList(manager.getBook());
+  } else if (e.target.closest('.btn-read-un')) {
+    manager.findBook(li.id);
+    makeList(manager.getBook());
+  } else if (e.target.closest('.btn-edit')) {
+    editMode(oneBook);
+  } else if (e.target.closest('.finish-edit')) {
+    makeList(books);
+  } else if (e.target.closest('.delete-btn')) {
+    manager.deleteBook(li.id);
+    makeList(manager.getBook());
+  } else if (e.target.closest('.btn-read_un')) {
+    manager.findBook(li.id);
+    editMode(oneBook);
+  }
+});
 function marginTransition() {
   textBox.style.marginTop = '50px';
   textBox.style.transition = '0.5s';
   textBoxBottom.style.marginBottom = '130px';
 }
-ulBox.addEventListener('click', function (e) {
-  const li = e.target.closest('.list-item');
-  const books = manager.getBook();
-  if (e.target.closest('.btn-delete')) {
-    manager.deleteBook(li.id);
-    makeList(books);
-  } else if (e.target.closest('.btn-read-un')) {
-    manager.findBook(li.id);
-    makeList(books);
-  } else if (e.target.closest('.btn-edit')) {
-    editMode(books);
-  } else if (e.target.closest('.finish-edit')) {
-    //const boo = manager.getBook();
-    //const boo = books.find(b => b.getId() === li.id);
-    const creator = bookCreator(titleB, autorB, nPages, check);
-    const nameInput = li.querySelector('.input-mode-edit-name');
-    const autorInput = li.querySelector('.input-mode-edit-autor');
-    const pagesInput = li.querySelector('.input-mode-edit-pagesnumber');
-
-    creator.updateName(nameInput.value);
-    creator.updateAutor(autorInput.value);
-    creator.updateNumberOfPages(pagesInput.value);
-
-    makeList(books);
-  }
-});
-function editMode(books) {
-  ulBox.innerHTML = '';
-  books.forEach(function (book) {
-    const html = `<li class="list-item" id=${book.getId()}>
-            <input type="text" class="input-mode-edit-name">
-            <input type="text" class="input-mode-edit-autor">
-            <input type="text" class="input-mode-edit-pagesnumber">
-            <div class="card-btn">
-              <button type="button" class="btn-read-un">${
-                book.getIsCheck() ? 'Read ✔️' : 'Unread ❌'
-              }</button>
-              <button type="button" class="btn-delete">Delete Book</button>
-              <button type="button" class="finish-edit">Finish Editing</button>
-            </div>`;
-    ulBox.insertAdjacentHTML('afterbegin', html);
-    //
-    const name = document.querySelector('.input-mode-edit-name');
-    const autor = document.querySelector('.input-mode-edit-autor');
-    const numberOfPages = document.querySelector(
-      '.input-mode-edit-pagesnumber'
-    );
-    name.value = `${book.getName()}`;
-    autor.value = `${book.getAutor()}`;
-    numberOfPages.value = `${book.getNumberOfPages()}`;
-
-    //const btnFinish = document.querySelector('.finish-edit');
-    //btnFinish.addEventListener('click', makeList(books));
-    form.classList.add('hidden');
-    overlay.classList.add('hidden');
-    marginTransition();
-  });
+function addMargin() {
+  textBox.style.marginTop = '150px';
+  textBox.style.transition = '0.5s';
+  textBoxBottom.style.marginBottom = '100px';
 }
-/*dodati jos jedno dugme na knjigu edit
-kad se to dugme stisne na mesto gde su bili naslov... treba da se pojave inputi koji ce imati te vrednosti
-Nakon sto se edit stisne to dugme edit treba da se pretvori u dugme finishEditing
-A kad se ono stisne knjiga se vraca na staro sa novim vrednostima
- */
